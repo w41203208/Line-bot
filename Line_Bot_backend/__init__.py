0704fc558d625dd5f5b8e78135547cc7b682b36d
Jay <w41203208@gmail.com>
2021-11-11 04:08:51 +0800

    no message

--- Line_Bot_backend/__init__.py
+++ Line_Bot_backend/__init__.py
@@ -1,10 +1,10 @@
 import os
 from flask import Flask
-import pymysql
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+#import pymysql
+#from flask_sqlalchemy import SQLAlchemy
+#from flask_migrate import Migrate
 
-pymysql.install_as_MySQLdb()
+#pymysql.install_as_MySQLdb()
 #db = SQLAlchemy()
 #migrate = Migrate()
 
