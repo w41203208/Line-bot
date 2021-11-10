f55f86e6965731b76138b1c0f405102aca68bcc6
Jay <w41203208@gmail.com>
2021-11-11 03:40:27 +0800

    去掉資料庫的部分

--- Line_Bot_backend/__init__.py
+++ Line_Bot_backend/__init__.py
@@ -5,8 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 pymysql.install_as_MySQLdb()
-db = SQLAlchemy()
-migrate = Migrate()
+#db = SQLAlchemy()
+#migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -15,11 +15,11 @@ def create_app(test_config=None):
     app.config['JWT_SECRET_KEY'] = 'this-should-be-change'
 
     ######## Register Database ########
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:joyce50635@127.0.0.1:3306/kcs_database'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
-    migrate.init_app(app, db)
-    db.create_all(app=app)
+    #app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:joyce50635@127.0.0.1:3306/kcs_database'
+    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    #db.init_app(app)
+    #migrate.init_app(app, db)
+    #db.create_all(app=app)
 
 
     from .views import views
