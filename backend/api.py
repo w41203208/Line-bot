3212d350b2be5f6e21687fe5af99a3ba0f78b91b
w41203208@gmail.com <w41203208@gmail.com>
2022-02-15 22:13:34 +0800

    update

--- backend/api.py
+++ backend/api.py
@@ -20,20 +20,90 @@ class GETfoodDataAPI():
         colorNa, colorP, colorKa, colorTitle, nameTitle = '', '', '', '', ''
 
         for item in query_data:
-            proteinDesc = '無任何建議'
+            noticeDict = {}
+            noticeFormat = []
+
             if item.protein:
                 proteinDesc = item.protein.proteinDesc
+                noticeDict[0] = {
+                    'descName': proteinDesc,
+                    'isDesc': True,
+                }
+            else :
+                proteinDesc = ' '
+                noticeDict[0] = {
+                    'descName': proteinDesc,
+                    'isDesc': False,
+                }
 
-            colorNa = '#FF0000' if item.foodNaa > 700 else '#888888'
-            colorP = '#FF0000' if item.foodP > 250 else '#888888'
-            colorKa = '#FF0000' if item.foodKa > 300 else '#888888'
 
+            ###判斷底下顯示內容與值得顏色###
+            if item.foodNaa > 700:
+                colorNa = '#FF0000'
+                noticeDict[1] = {
+                    "descName": "為高鹽份食品，要避免吃。此外，少外食，少吃加工食品，養成清淡飲食習慣。",
+                    "isDesc": True,
+                }
+            else:
+                colorNa = '#888888'
+                noticeDict[1] = {
+                    "descName": " ",
+                    "isDesc": False,
+                }
+
+            if item.foodP > 250:
+                colorP = '#FF0000'
+                noticeDict[2] = {
+                    "descName": "為高磷食品，要避免吃。常見堅果類、奶類製品、動物內臟、加工食品，也為高磷食物要避免吃。",
+                    "isDesc": True,
+                }
+            else:
+                colorP = '#888888'
+                noticeDict[2] = {
+                    "descName": " ",
+                    "isDesc": False,
+                }
+
+            if item.foodKa > 300:
+                colorKa = '#FF0000'
+                noticeDict[3] = {
+                    "descName": "為高鉀食品，要避免吃。建議吃蔬菜前先川燙，菜湯不喝。全穀物、果乾、乾香菇避免吃。",
+                    "isDesc": True,
+                }
+            else:
+                colorKa = '#888888'
+                noticeDict[3] = {
+                    "descName": " ",
+                    "isDesc": False,
+                }
+
+            for i in noticeDict:
+                if noticeDict[i]['isDesc'] != False:
+                    noticeFormat.append({
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": noticeDict[i]['descName'],
+                                "wrap": True,
+                                "weight": "bold",
+                                "color": "#000000"
+                            }
+                        ],
+                    })
+
+
+            ####判斷標題####
             if colorNa == '#FF0000' or colorP == '#FF0000' or colorKa == '#FF0000':
                 colorTitle = '#FF0000'
-                nameTitle = '不建議吃!!'
+                nameTitle = '不建議吃'
+                imageUrl = "https://upload.cc/i1/2022/02/13/Dk4q3b.png"
             else:
-                colorTitle = '#008000'
-                nameTitle = '建議吃!!'
+                colorTitle = '#000000'
+                nameTitle = '可安全食用'
+                imageUrl = "https://upload.cc/i1/2022/02/13/uNfmWz.png"
+
 
 
             flexMessage_bubble = {
@@ -50,8 +120,8 @@ class GETfoodDataAPI():
                             "type": "text",
                             "text": nameTitle,
                             "align": "center",
-                            "offsetStart": "15px",
-                            "size": "xl",
+                            "offsetStart": "17px",
+                            "size": "xxl",
                             "weight": "bold",
                             "color": colorTitle,
                             }
@@ -80,15 +150,6 @@ class GETfoodDataAPI():
                                 "size": "xl",
                                 "weight": "bold",
                                 "flex": 7
-                                },
-                                {
-                                "type": "text",
-                                "text": "每份 100公克",
-                                "wrap": True,
-                                "color": "#888888",
-                                "flex": 4,
-                                "gravity": "bottom",
-                                "align": "end"
                                 }
                             ]
                             },
@@ -197,24 +258,30 @@ class GETfoodDataAPI():
                                     "color": "#888888"
                                     }
                                 ]
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "horizontal",
+                                    "contents": [
+                                        {
+                                            "type": "text",
+                                            "text": "每份 100公克",
+                                            "wrap": True,
+                                            "color": "#888888",
+                                            "flex": 4,
+                                            "gravity": "bottom",
+                                        }
+                                    ],
+                                    "paddingTop": "20px",
                                 }
                             ],
-                            "paddingTop": "20px"
+                            "paddingTop": "10px"
                             },
                             {
                             "type": "box",
-                            "layout": "horizontal",
-                            "contents": [
-                                {
-                                "type": "text",
-                                "text": proteinDesc,
-                                "wrap": True,
-                                "weight": "bold",
-                                "color": "#000000"
-                                }
-                            ],
+                            "layout": "vertical",
+                            "contents": noticeFormat,
                             "paddingTop": "15px",
-                            "height": "100px"
                             }
                         ],
                         "paddingStart": "20px",
@@ -223,13 +290,13 @@ class GETfoodDataAPI():
                         "paddingBottom": "20px"
                         },
                         {
-                        "type": "box",
-                        "layout": "vertical",
-                        "contents": [],
-                        "width": "80px",
-                        "height": "80px",
-                        "backgroundColor": "#cccccc",
-                        "position": "absolute"
+                            "type": "image",
+                            "url": imageUrl,
+                            "aspectMode": "cover",
+                            "position": "absolute",
+                            "offsetStart": "-20px",
+                            "size": "lg",
+                            "offsetTop": "-35px"
                         }
                     ],
                     "paddingAll": "0px"
